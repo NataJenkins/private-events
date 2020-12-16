b794f576807bd7a62e0a26427f4a0723d669cb68
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2020-12-16 11:42:17 -0600

    Revert "devise & scaffold"
    This reverts commit 3cb4fac5e0a1fc5ecb8da818d08ae37844cb6984.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  resources :events
-  resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "events#index"
 end
