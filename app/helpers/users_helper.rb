03b3171e59a89d2406ae87758f81615dc6eafbb5
Nexch <mauricio.t.1999@gmail.com>
2021-01-11 09:29:50 -0500

    fix all errors with TSE

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,37 @@
 module UsersHelper
+  def timer(creator, user, c_date, lol)
+    if creator == user
+      if c_date > Time.zone.now
+
+      else
+        return lol
+      end
+    else
+
+    end
+  end
+
+  def timer2(creator, user, c_date, lol)
+    if creator == user
+      if c_date > Time.zone.now
+        lol
+      else
+
+      end
+    else
+
+    end
+  end
+
+  def if_nil(lol)
+    array = Array.new
+    lol.each do |meme|
+      if meme != nil
+        array.push(meme)
+      else
+
+      end
+    end
+    return array
+  end
 end
