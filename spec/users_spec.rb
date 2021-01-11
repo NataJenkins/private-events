75ed3ca11da77372ef44c4e6075e278d1e226a92
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-01-11 14:42:45 -0600

    tests added

--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Creating a new user', type: :feature do
+RSpec.describe 'user', type: :feature do
   scenario 'sign up' do
     visit new_user_registration_path
     fill_in 'Email', with: 'test1@test.com'
@@ -10,4 +10,25 @@ RSpec.describe 'Creating a new user', type: :feature do
 
     expect(page).to have_content('Welcome! You have signed up successfully.')
   end
+
+  scenario 'logout' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'test1@test.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+    click_on 'Sign up'
+    click_on 'Logout'
+
+    expect(page).to have_content('Signed out successfully.')
+  end
+
+  scenario 'invalid' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'a@a.com'
+    fill_in 'Password', with: '111111'
+    click_on 'Log in'
+
+    expect(page).to have_content('Invalid Email or password.')
+  end
+
 end
