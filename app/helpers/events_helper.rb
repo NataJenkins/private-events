e7a248e8cd147fdbf694fca471c4ccb0e7a3f901
Nexch <mauricio.t.1999@gmail.com>
2021-01-12 06:27:51 -0500

    remove logic from user and events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,24 @@
 module EventsHelper
+  def timer(creator, user, c_date, lol)
+    if creator == user
+      if c_date > Time.zone.now
+        nil
+      else
+        lol
+      end
+    end
+  end
+
+  def timer2(creator, user, c_date, lol)
+    lol if creator == user && c_date > Time.zone.now
+  end
+
+  def if_nil(lol)
+    array = []
+    lol.each do |meme|
+      array.push(meme) unless meme.nil?
+    end
+    array
+  end
+
 end
