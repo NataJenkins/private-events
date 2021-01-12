6db27b7a9881abdbab544fd3df5b75e37de894b2
Nexch <mauricio.t.1999@gmail.com>
2021-01-12 12:01:45 -0500

    fix event and user index page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,36 @@
-# rubocop:disable Style/GuardClause
-
 module UsersHelper
-  def timer(creator, user, c_date, lol)
-    if creator == user
-      if c_date > Time.zone.now
-        nil
-      else
-        lol
-      end
+  #  def timer(creator, user, c_date, lol)
+  #    if creator == user
+  #      if c_date > Time.zone.now
+  #        nil
+  #      else
+  #        lol
+  #      end
+  #    end
+  #  end
+  #
+  #  def timer2(creator, user, c_date, lol)
+  #    lol if creator == user && c_date > Time.zone.now
+  #  end
+  #
+  #  def if_nil(lol)
+  #    array = []
+  #    lol.each do |meme|
+  #      array.push(meme) unless meme.nil?
+  #    end
+  #    array
+  #  end
+
+  def timer_e(c_date, lol)
+    if c_date > Time.zone.now
+      nil
+    else
+      lol
     end
   end
 
-  def timer2(creator, user, c_date, lol)
-    lol if creator == user && c_date > Time.zone.now
+  def timer2_e(c_date, lol)
+    lol if c_date > Time.zone.now
   end
 
   def if_nil(lol)
@@ -23,5 +41,3 @@ module UsersHelper
     array
   end
 end
-
-# rubocop:enable Style/GuardClause
